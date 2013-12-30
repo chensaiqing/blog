6a2fbad54ff0d6762fcc31de5fbda37c872155be
chensaiqing <chensaiqing@91waijiao.com>
2013-12-30 12:45:31 +0800

    add edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,19 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+  
+  def edit
+    @post = Post.find(params[:id])
+  end  
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(params[:post].permit(:title, :text))
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
 
   def index
     @post = Post.all
