84f1f2650f721eb9cf7824584c58ae22c15aef1b
chensaiqing <chensaiqing@91waijiao.com>
2013-12-30 13:03:42 +0800

    add destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,12 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
   
   def edit
     @post = Post.find(params[:id])
